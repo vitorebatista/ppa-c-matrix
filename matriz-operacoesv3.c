a0fdcc10061046a830ea497b4eac8ed85cb17e3a
Vitor Emanuel Batista <vitorebatista@gmail.com>
2019-09-07 03:39:42 +0000

    lint

--- matriz-operacoesv3.c
+++ matriz-operacoesv3.c
@@ -1,73 +1,77 @@
-#include "matrizv3.h"
 #include "toolsv3.h"
+#include "matrizv3.h"
 
 /*
-function msomar
 Realiza a soma de duas matrizes.
 As matrizes devem ter o mesmo número de linhas e colunas, sendo o
 resultado uma nova matriz de mesmo tamanho.
 @return res, ponteiro para a matriz resultante
-@param mat_a, ponteiro para objeto do tipo mymatriz
-@param mat_b, ponteiro para objeto do tipo mymatriz
-@param tipo, inteiro que define a ordem de aninhamento dos loops:
-             - 0 para ij
-             - 1 para ji
+@param mat_a, ponteiro da struct mymatriz
+@param mat_b, ponteiro da struct mymatriz
+@param tipo, ordem para executar a somatória:
+        - 0 para ij
+        - 1 para ji
 */
-mymatriz *msomar (mymatriz *mat_a, mymatriz *mat_b, int tipo){
+mymatriz *msomar(mymatriz *mat_a, mymatriz *mat_b, int tipo)
+{
     mymatriz temp;
     mymatriz *result = malloc(sizeof(mymatriz));
-    int i_max, j_max; //auxiliares para controle de aninhamento
+    int i_max, j_max;
 
-    //verifica se foi alocado memória para a matriz
-    if ( (mat_a == NULL) || (mat_b == NULL)) {
-        printf ("** Erro: Memoria Insuficiente **\n");
-        return NULL;
+    if (mat_a->matriz == NULL)
+    {
+        printf("\nERRO: Matriz A não alocada \n\n");
+        exit(1);
     }
 
-    //valida se matrizes tem tamanhos compatíveis
-    if (mat_a->col != mat_b->col || mat_a->lin != mat_b->lin ){
-        printf ("** Erro: Matrizes devem ter mesma configuração para que se possa somar. **\n");
-        return NULL;
+    if (mat_b->matriz == NULL)
+    {
+        printf("\nERRO: Matriz B não alocada \n\n");
+        exit(1);
     }
 
-    //matriz resultado
-	temp.matriz = NULL;
-	temp.lin = mat_a->lin;
-	temp.col = mat_a->col;
+    if ((mat_a->lin != mat_b->lin) || (mat_a->col != mat_b->col))
+    {
+        printf("\nERRO: As matrizes não são do mesmo tamanho\n");
+        exit(1);
+    }
 
-    //realiza a alocação de memória para matriz resultado
-    if (malocar(&temp)) {
-		printf ("ERROR: Out of memory\n");
-	}else{
+    temp.matriz = NULL;
+    temp.lin = mat_a->lin;
+    temp.col = mat_a->col;
+
+    if (malocar(&temp))
+    {
+        printf("\nERROR: Erro ao inicializar matriz\n");
+        exit(1);
+    }
+    else
+    {
         mzerar(&temp);
     }
 
     //define aninhamento, conforme parametro tipo
     i_max = mat_a->lin;
     j_max = mat_a->col;
-    if (tipo == 1){ //tipo = 1: ordem de aninhamento ji
+    if (tipo == 1)
+    { //tipo = 1: ordem de aninhamento ji
         i_max = mat_a->col;
         j_max = mat_a->lin;
     }
-    
+
     //realiza a soma dos elementos da matriz a e b
-    for (int i = 0; i < i_max; i++){
-        
-        for (int j = 0; j < j_max; j++){
-            // printf("[i] %d ", i );
-            // printf("[j] %d ", j );
-            if (tipo == 0){ //tipo = 0: ordem de aninhamento ij
-                // printf("[a] %d ", mat_a->matriz[i][j] );
-                // printf("[b] %d ", mat_b->matriz[i][j]);
+    for (int i = 0; i < i_max; i++)
+    {
+        for (int j = 0; j < j_max; j++)
+        {
+            if (tipo == 0)
+            { //tipo = 0: ordem de aninhamento ij
                 temp.matriz[i][j] = mat_a->matriz[i][j] + mat_b->matriz[i][j];
-                // printf("[r] %d\n", result.matriz[i][j]);
-            }else{ //tipo = 1: ordem de aninhamento ji
-                // printf("[a1] %d ", mat_a->matriz[i][j] );
-                // printf("[b1] %d ", mat_b->matriz[i][j]);
-                temp.matriz[j][i] = mat_a->matriz[j][i] + mat_b->matriz[j][i];
-                // printf("[r1] %d\n", result.matriz[i][j]);
             }
-            
+            else
+            { //tipo = 1: ordem de aninhamento ji
+                temp.matriz[j][i] = mat_a->matriz[j][i] + mat_b->matriz[j][i];
+            }
         }
     }
     result->matriz = temp.matriz;
@@ -76,9 +80,7 @@ mymatriz *msomar (mymatriz *mat_a, mymatriz *mat_b, int tipo){
     return result;
 }
 
-
 /*
-function mmultiplicar
 Realiza a multiplicação de duas matrizes.
 As matrizes envolvidas na multiplicação devem seguir a regra: número de colunas da primeira
 matriz deve ser igual ao número de linhas da segunda matriz. A matriz resultante será configurada
@@ -86,7 +88,7 @@ tendo o número de linhas da primeira e o número de colunas da segunda. Ex: 3x4
 @return res, ponteiro para a matriz resultante
 @param mat_a, ponteiro para objeto do tipo mymatriz
 @param mat_b, ponteiro para objeto do tipo mymatriz
-@param tipo, inteiro que define a ordem de aninhamento dos loops:
+@param tipo, ordem para executar a multiplicação:
              - 0 para ijk
              - 1 para ikj
              - 2 para kij
@@ -95,91 +97,100 @@ tendo o número de linhas da primeira e o número de colunas da segunda. Ex: 3x4
              - 5 para jki
 
 */
-mymatriz *mmultiplicar (mymatriz *mat_a, mymatriz *mat_b, int tipo){    
+mymatriz *mmultiplicar(mymatriz *mat_a, mymatriz *mat_b, int tipo)
+{
     mymatriz temp;
     mymatriz *result = malloc(sizeof(mymatriz));
     int i_max, j_max, k_max; //auxiliares para controle de aninhamento
-    
-    //verifica se foi alocado memória para a matriz
-    if ( (mat_a == NULL) || (mat_b == NULL)) {
-        printf ("** Erro: Memoria Insuficiente **\n");
-        return NULL;
+
+    if (mat_a->matriz == NULL)
+    {
+        printf("\nERRO: Matriz A não alocada \n\n");
+        exit(1);
     }
 
-    //valida se matrizes tem tamanhos compatíveis
-    if (mat_a->col != mat_b->lin ){
-        printf ("** Erro: Matrizes devem ter mesma configuração para que se possa somar. **\n");
-        return NULL;
+    if (mat_b->matriz == NULL)
+    {
+        printf("\nERRO: Matriz B não alocada \n\n");
+        exit(1);
+    }
+
+    if ((mat_a->lin != mat_b->lin) || (mat_a->col != mat_b->col))
+    {
+        printf("\nERRO: As matrizes não são do mesmo tamanho\n");
+        exit(1);
     }
 
     //matriz resultado
-	temp.matriz = NULL;
-	temp.lin = mat_a->lin;
-	temp.col = mat_b->col;
-    
+    temp.matriz = NULL;
+    temp.lin = mat_a->lin;
+    temp.col = mat_b->col;
+
     //realiza a alocação de memória para matriz resultado
-    if (malocar(&temp)) {
-		printf ("ERROR: Out of memory\n");
-	}else{
+    if (malocar(&temp))
+    {
+        printf("\nERROR: Erro ao inicializar matriz\n");
+        exit(1);
+    }
+    else
+    {
         mzerar(&temp);
     }
-    printf("lin %d col %d\n", temp.lin, temp.col);
-    //inicializa variáveis de controle dos for`s
+
     i_max = mat_a->lin;
     j_max = mat_b->col;
-    k_max = mat_a->col; //ou mat_b->lin
+    k_max = mat_a->col;
 
-    //define aninhamento, conforme parametro tipo
     int maxA = 0;
     int maxB = 0;
     int maxC = 0;
-    
-    switch(tipo){
-        case 0: //ijk
-            maxA = i_max;
-            maxB = j_max;
-            maxC = k_max;
-            break;
 
-        case 1: //ikj
-            maxA = i_max;
-            maxB = k_max;
-            maxC = j_max;
-            break;
+    switch (tipo)
+    {
+    case 0: //ijk
+        maxA = i_max;
+        maxB = j_max;
+        maxC = k_max;
+        break;
 
-        case 2: //kij
-            maxA = k_max;
-            maxB = i_max;
-            maxC = j_max;
-            break;
+    case 1: //ikj
+        maxA = i_max;
+        maxB = k_max;
+        maxC = j_max;
+        break;
 
-        case 3: //kji
-            maxA = k_max;
-            maxB = j_max;
-            maxC = i_max;
-            break;
+    case 2: //kij
+        maxA = k_max;
+        maxB = i_max;
+        maxC = j_max;
+        break;
 
-        case 4: //jik
-            maxA = j_max;
-            maxB = i_max;
-            maxC = k_max;
-            break;
+    case 3: //kji
+        maxA = k_max;
+        maxB = j_max;
+        maxC = i_max;
+        break;
 
-        default: //jki
-            maxA = j_max;
-            maxB = k_max;
-            maxC = i_max;
-            break;
-            
+    case 4: //jik
+        maxA = j_max;
+        maxB = i_max;
+        maxC = k_max;
+        break;
+
+    default: //jki
+        maxA = j_max;
+        maxB = k_max;
+        maxC = i_max;
+        break;
     }
 
-    for (int a = 0; a < maxA; a++){
-        for (int b = 0; b < maxB; b++){
-            for (int c = 0; c < maxC; c++){
+    for (int a = 0; a < maxA; a++)
+    {
+        for (int b = 0; b < maxB; b++)
+        {
+            for (int c = 0; c < maxC; c++)
+            {
                 temp.matriz[a][b] += mat_a->matriz[a][c] * mat_b->matriz[c][b];
-                // printf("a[%d][%d] = %d\n", a, c, mat_a->matriz[a][c] );
-                // printf("b[%d][%d] = %d\n", c, b, mat_b->matriz[c][b] );
-                // printf("%d x %d = %d (%d)\n", mat_a->matriz[a][c], mat_b->matriz[c][b], mat_a->matriz[a][c] * mat_b->matriz[c][b], result.matriz[a][b]);
             }
         }
     }
--- matrizv3.c
+++ matrizv3.c
@@ -14,26 +14,30 @@ Conditional jump or move depends on uninitialised value(s)
 https://cs50.stackexchange.com/questions/28837/conditional-jump-or-move-depends-on-uninitialized-value
 
 */
-int malocar(mymatriz *matriz) {
-    int **newMatrix = NULL;
+int malocar(mymatriz *matriz)
+{
+	int **newMatrix = NULL;
 
-	printf("\nmalocar =[%d,%d]\n",matriz->lin, matriz->col);
-	newMatrix = (int **) calloc(matriz->lin, sizeof(int *));
+	printf("\nmalocar =[%d,%d]\n", matriz->lin, matriz->col);
+	newMatrix = (int **)calloc(matriz->lin, sizeof(int *));
 	//newMatrix = (int **) malloc(matrix->lin * sizeof(int *));
-	if (!newMatrix) {
-		printf("ERROR: Out of memory 1\n");
+	if (!newMatrix)
+	{
+		printf("ERROR: Erro ao inicializar matriz [1]\n");
 		return -4;
 	}
 
-  	for (int lin =0; lin < matriz->lin; lin++) {
-			newMatrix[lin] = (int *) calloc(matriz->col, sizeof(int));
-			//newMatrix[lin] = (int *) malloc(sizeof(int)*matriz->col);
-			if (!newMatrix) {
-				printf("ERROR: Out of memory 2\n");
-				return -4;
-			}
+	for (int lin = 0; lin < matriz->lin; lin++)
+	{
+		newMatrix[lin] = (int *)calloc(matriz->col, sizeof(int));
+		//newMatrix[lin] = (int *) malloc(sizeof(int)*matriz->col);
+		if (!newMatrix)
+		{
+			printf("ERROR: Erro ao inicializar matriz [2]\n");
+			return -4;
+		}
 	}
-    matriz->matriz = newMatrix;
+	matriz->matriz = newMatrix;
 	return 0;
 }
 
@@ -44,16 +48,18 @@ Gera valores dentro de uma matriz já alocada
 @param valor, int, Menor ideia do que pode ser este parâmetro
 
 */
-int mgerar(mymatriz *matriz, int valor){
-	
-	int value = 0;// Controlador de valor
-	
-	if ( matriz->matriz == NULL ){
+int mgerar(mymatriz *matriz, int valor)
+{
+
+	int value = 0; // Controlador de valor
+
+	if (matriz->matriz == NULL)
+	{
 		printf("\nERRO: Matriz não alocada \n\n");
 		return -3;
 	}
 
-    /* srand(wtime(NULL)) objetiva inicializar o gerador de números aleatórios
+	/* srand(wtime(NULL)) objetiva inicializar o gerador de números aleatórios
     com o valor da função wtime(NULL). Este por sua vez, é calculado
     como sendo o total de segundos passados desde 1 de janeiro de 1970
     até a data atual.
@@ -63,24 +69,26 @@ int mgerar(mymatriz *matriz, int valor){
 	srand(wtime(NULL));
 
 	// Percorre todas as linhas da matriz
-	for (int lin=0; lin < matriz->lin; lin++) {
+	for (int lin = 0; lin < matriz->lin; lin++)
+	{
 		// Percorre todas as Colunas da Matriz
-		for (int col=0; col < matriz->col; col++) {
+		for (int col = 0; col < matriz->col; col++)
+		{
 
 			// Verifica o valor passado por parâmetro
 			switch (valor)
 			{
-				case -9999: // Menor ideia do que pode ser este valor negativo
-					value = rand() % 100;
-					break;
-				case 0: // valor=0 para zera os valores
-					value = 0;
-					break;
-				default:// 
-					value = valor;
-					break;
+			case -9999: // Menor ideia do que pode ser este valor negativo
+				value = rand() % 100;
+				break;
+			case 0: // valor=0 para zera os valores
+				value = 0;
+				break;
+			default: //
+				value = valor;
+				break;
 			}
-			
+
 			// Atribui o valor a posição da Matriz
 			matriz->matriz[lin][col] = value;
 		}
@@ -88,30 +96,33 @@ int mgerar(mymatriz *matriz, int valor){
 	return 0;
 }
 
-
 /*
 Imprime os valores da Matriz
 */
-int mimprimir(mymatriz *matriz){
+int mimprimir(mymatriz *matriz)
+{
 
-	if ( matriz->matriz == NULL ){
+	if (matriz->matriz == NULL)
+	{
 		printf("\nERRO: Matriz não alocada \n\n");
 		exit(1);
 	}
 
 	// Realiza uma impressão de primeira linha com posição das colunas
-	for (int col=0; col < matriz->col; col++)
+	for (int col = 0; col < matriz->col; col++)
 		printf("\t(%d)", col);
 
 	// Pula linha
 	printf("\n");
 
 	// Percorre as linhas da Matriz
-	for (int lin=0; lin < matriz->lin; lin++) {
+	for (int lin = 0; lin < matriz->lin; lin++)
+	{
 		// Imprime no começo da linha a posição
 		printf("(%d)", lin);
 		// Percorre as colunas da Matriz
-	  	for (int col=0; col < matriz->col; col++){
+		for (int col = 0; col < matriz->col; col++)
+		{
 			// Imprime os valores contidos na Matriz
 			printf("\t %d", matriz->matriz[lin][col]);
 		}
@@ -121,12 +132,13 @@ int mimprimir(mymatriz *matriz){
 	return 0;
 }
 
-
 /*
 Gera valores zerados em uma matriz já alocada
 */
-int mzerar (mymatriz *matriz){
-	if ( matriz->matriz == NULL ){
+int mzerar(mymatriz *matriz)
+{
+	if (matriz->matriz == NULL)
+	{
 		printf("\nERRO: Matriz não alocada \n\n");
 		exit(1);
 	}
@@ -134,16 +146,17 @@ int mzerar (mymatriz *matriz){
 	return mgerar(matriz, 0);
 }
 
-
 /*
 Imprime os valores da matriz
 */
-int mliberar (mymatriz *matriz) {
-	
+int mliberar(mymatriz *matriz)
+{
+
 	// Só necessita liberar Matrizes alocadas
-	if (matriz->matriz) {
+	if (matriz->matriz)
+	{
 		// Libera as posições de cada Linha
-		for (int lin =0; lin < matriz->lin; lin++)
+		for (int lin = 0; lin < matriz->lin; lin++)
 			free(matriz->matriz[lin]);
 
 		// Libera a Matriz completa
@@ -156,29 +169,35 @@ int mliberar (mymatriz *matriz) {
 /*
 Realiza a comparação de duas Matrizes
 */
-int mcomparar (mymatriz *mat_a, mymatriz *mat_b){
+int mcomparar(mymatriz *mat_a, mymatriz *mat_b)
+{
 	double time1, time2;
 
-	if ( mat_a->matriz == NULL ){
+	if (mat_a->matriz == NULL)
+	{
 		printf("\nERRO: Matriz A não alocada \n\n");
 		return -3;
 	}
 
-	if ( mat_b->matriz == NULL ){
+	if (mat_b->matriz == NULL)
+	{
 		printf("\nERRO: Matriz B não alocada \n\n");
 		return -3;
 	}
-	
-	
-	if ((mat_a->lin != mat_b->lin) || (mat_a->col != mat_b->col)) {
+
+	if ((mat_a->lin != mat_b->lin) || (mat_a->col != mat_b->col))
+	{
 		printf("\nAs matrizes não são do mesmo tamanho\n");
 		return -2;
 	}
 
 	time1 = wtime();
-	for (int lin=0; lin < mat_a->lin; lin++) {
-	  for (int col=0; col < mat_a->col; col++){
-			if (mat_a->matriz[lin][col] != mat_b->matriz[lin][col]) {
+	for (int lin = 0; lin < mat_a->lin; lin++)
+	{
+		for (int col = 0; col < mat_a->col; col++)
+		{
+			if (mat_a->matriz[lin][col] != mat_b->matriz[lin][col])
+			{
 				printf("\nO elemento [%d,%d] é diferente nas matrizes analisadas!\n", lin, col);
 				printf("Os valores são %d e %d\n", mat_a->matriz[lin][col], mat_b->matriz[lin][col]);
 				return -1;
@@ -186,10 +205,7 @@ int mcomparar (mymatriz *mat_a, mymatriz *mat_b){
 		}
 	}
 	time2 = wtime();
-	printf ("\nMatrizes são idênticas!! :)\n");
-	printf ("Tempo de processamento: %f\n", time2 - time1);
+	printf("\nMatrizes são idênticas!! :)\n");
+	printf("Tempo de processamento: %f\n", time2 - time1);
 	return 0;
 }
-
-
-
