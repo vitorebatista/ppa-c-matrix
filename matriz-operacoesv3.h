b77170e66a873d27f32afbf219477aef76ba10fa
Vitor Emanuel Batista <vitorebatista@gmail.com>
2019-09-21 23:48:34 +0000

    change func

--- matriz-operacoesv3.h
+++ matriz-operacoesv3.h
@@ -29,3 +29,5 @@ mymatriz *msomar (mymatriz *mat_a, mymatriz *mat_b, int tipo);
 mymatriz *mmultiplicar (mymatriz *mat_a, mymatriz *mat_b, int tipo);
 
 int multiplicar_submatriz (matriz_bloco_t *mat_suba, matriz_bloco_t *mat_subb, matriz_bloco_t *mat_subc);
+matriz_bloco_t **csubmatrizv2(int mat_lin, int mat_col, int divisor);
+matriz_bloco_t **particionar_matriz(int **matriz, int mat_lin, int mat_col, int orientacao, int divisor);
--- matrizv3.h
+++ matrizv3.h
@@ -37,8 +37,8 @@ int mcomparar (mymatriz *mat_a, mymatriz *mat_b);
 // submatriz
 //int gerar_submatriz (int **mat_origem, matriz_bloco_t *submatriz, bloco_t *bloco);
 //int imprimir_submatriz (matriz_bloco_t *submatriz);
-matriz_bloco_t **particionar_matriz (int **matriz, int mat_lin, int mat_col, int orientacao, int divisor);
+//matriz_bloco_t **particionar_matriz (int **matriz, int mat_lin, int mat_col, int orientacao, int divisor);
 //matriz_bloco_t **liberar_submatriz (matriz_bloco_t **submatriz);
 //int imprimir_bloco (matriz_bloco_t *submatriz);
 //matriz_bloco_t **constroi_submatriz (int **matriz, int mat_lin, int mat_col, int divisor);
-matriz_bloco_t **csubmatrizv2 (int mat_lin, int mat_col, int divisor);
+//matriz_bloco_t **csubmatrizv2 (int mat_lin, int mat_col, int divisor);
