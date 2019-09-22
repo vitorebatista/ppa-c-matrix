41a057954509625e9241fad149ca937388e9f971
Vitor Emanuel Batista <vitorebatista@gmail.com>
2019-09-22 00:34:01 +0000

    fix toolsv3

--- toolsv3.h
+++ toolsv3.h
@@ -5,24 +5,24 @@
 #ifndef SOME_HEADER_FILE_H
 #define SOME_HEADER_FILE_H
 typedef struct {
-  int lin_inicio;
-  int lin_fim;
-  int col_inicio;
-  int col_fim;
+    int lin_inicio;
+    int lin_fim;
+    int col_inicio;
+    int col_fim;
 } bloco_t;
 
 typedef struct {
-  int **matriz;
-  bloco_t *bloco;
-  int mat_col;
-  int mat_lin;
-  int divisor;
+    mymatriz *matriz; //Corrigido, estava int **matriz;
+    bloco_t *bloco;
+    int mat_col;
+    int mat_lin;
+    int divisor;
 } matriz_bloco_t;
 
 typedef struct {
-  int **matriz;
-  int lin;
-  int col;
+    int **matriz;
+    int lin;
+    int col;
 } mymatriz;
 #endif
 
